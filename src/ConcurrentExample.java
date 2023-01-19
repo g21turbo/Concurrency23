bea9c23b26cfc99867f9d388bbe280c983742988
User <dg21stew@gmail.com>
2023-01-19 12:11:28 -0800

    Initial commit

---
+++ src/ConcurrentExample.java
@@ -0,0 +1,34 @@
+public class ConcurrentExample {
+
+    public static void main(String[] args) {
+
+        // First task to run concurrently
+        Runnable task1 = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Task 1: " + i);
+                try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // Second task to run concurrently
+        Runnable task2 = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Task 2: " + i);
+                try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // Create two threads, one for each task
+        // and start them to run concurrently
+        new Thread(task1).start();
+        new Thread(task2).start();
+    }
+}
